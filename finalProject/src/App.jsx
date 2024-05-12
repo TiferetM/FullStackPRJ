df317379afc1d498a225c827811331b78f3ed784
yokheved <yokheved.spr@gmail.com>
2024-05-12 17:30:30 +0300

    fix

--- finalProject/src/App.jsx
+++ finalProject/src/App.jsx
@@ -1,36 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <>
       <header>
         <NavBar />
       </header>
       <Routes >
-        <Route path="">
-          <Route index element={<Welcome />} />
-          <Route path="login" element={<LogIn />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-        <Route path="/:id">
-          <Route index element={<Home />} />
-          <Route path="info" element={<Info />} />
-          <Route path="exit" element={<ExitPage />} />
-          <Route path="todos" element={<Todos />} />
-          <Route path="albums" >
-            <Route index element={<Albums />} />
-            <Route path=":albumId" element={<AlbumMain />} />
-          </Route>
-          <Route path="posts">
-            <Route index element={<Posts />} />
-            <Route path=":postId" element={<PostMain />} />
-          </Route>
-        </Route>
+        // Add the following code snippet to the Routes component in finalProject/src/App.jsx:
       </Routes>
     </>
   )
