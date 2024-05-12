5f78880adf0f1057c7802060c4624c7a4936a168
yokheved <yokheved.spr@gmail.com>
2024-05-12 17:14:00 +0300

    fix

--- finalProject/src/App.jsx
+++ finalProject/src/App.jsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <NavBar />
+      </header>
+      <Routes >
+        <Route path="">
+          <Route index element={<Welcome />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/:id">
+          <Route index element={<Home />} />
+          <Route path="info" element={<Info />} />
+          <Route path="exit" element={<ExitPage />} />
+          <Route path="todos" element={<Todos />} />
+          <Route path="albums" >
+            <Route index element={<Albums />} />
+            <Route path=":albumId" element={<AlbumMain />} />
+          </Route>
+          <Route path="posts">
+            <Route index element={<Posts />} />
+            <Route path=":postId" element={<PostMain />} />
+          </Route>
+        </Route>
+      </Routes>
     </>
   )
 }
