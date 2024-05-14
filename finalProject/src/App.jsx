750f8f3e2d86a3f7021b4c15ece0959ec1af4ac0
yokheved <yokheved.spr@gmail.com>
2024-05-14 16:59:24 +0300

    router running

--- finalProject/src/App.jsx
+++ finalProject/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
+import NavBar from './userComponents/navBar/NavBar'
+import { Routes } from 'react-router-dom'
 
 function App() {
     return (
     <>
       <header>
+        <h1>home design</h1>
         <NavBar />
       </header>
-      <Routes >
+      {/*<Routes >
         // Add the following code snippet to the Routes component in finalProject/src/App.jsx:
-      </Routes>
+    </Routes>*/}
     </>
   )
 }
