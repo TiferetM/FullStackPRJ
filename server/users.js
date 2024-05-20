23e1a4bf2959a3ff69ccb9eb01229c6a71f1b7c7
yokheved <yokheved.spr@gmail.com>
2024-05-20 16:56:42 +0300

    login

--- server/users.js
+++ server/users.js
@@ -1,5 +1,11 @@
 import { pool } from "../db/run.js";
 import express from 'express';
+import {sha512} from "js-sha512";
+
+// let str = "hello"
+// let secretKey = "MYSECRETKEY"
+
+// let hash = sha512.hmac(secretKey, str);
 
 export const app = express.Router(); // Exporting app as Router
 
@@ -8,5 +14,8 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.get('/login', (req, res) =>
 {
     const id=req.body.id;
-    
-}); //
+    const reqPassword=req.body.password;
+    const reqHash=sha512.hmac(id, reqPassword);
+    //get user from database and userHash
+    passwordHash==reqHash?res.send(user):res.send("failure");
+}); 
