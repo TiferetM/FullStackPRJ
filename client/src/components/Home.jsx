c88d66de3c8e76c5f775d0f0fc71a75afd8222d3
yokheved <yokheved.spr@gmail.com>
2024-06-02 20:26:15 +0300

    logo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
+import logo from '../images/logo.png'
+
 
 function home() {
   return (
     <>
         <div>home</div>
-        <img src={"./images/logo.png"} alt="Logo" />
+        {/* <img src={"../images/logo.png"} alt="Logo" /> this kind of image that will work */}
+        <img src={logo} alt="Logo" />
     </>
   )
 }
