7c44ca35eb53e9bf404dc297bed147e33d7fafec
yokheved <yokheved.spr@gmail.com>
2024-06-12 21:34:55 +0300

    nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,26 +10,12 @@ function NavBar({ userIn }) {
         const target = e.currentTarget;
         e.preventDefault();
         const links = navlinks(userIn, target.innerText);
-        console.log(links);
-        //if current target is not a nav link, return
-        if (target.tagName != "A" || links == undefined) return;
-        //if links is empty, return
-        if (Object.keys(links).length === 0) return;
-        //getBoundingClientRect returns the size of an element and its position relative to the viewport
-        // and its a function made by the browser
-        //the object returned will have the properties: x, y, width, height, top, right, bottom, left
-        //and to set those properties to the coordination object, we will use the spread operator
-        //and the command to set the properties target.set.style = {...getBoundingClientRect()} will not work
-        //because the object returned by getBoundingClientRect() is read-only
-        //so we will have to set the properties individually
-        //like this: target.style.left = `${coord.x}px`
-        //and this: target.style.bottom = `${coord.y}px`
+        if (target.tagName != "A" || Object.keys(links).length === 0) return;
         let coord = target.getBoundingClientRect();
         setHoverNav(cur => {
             return {
                 show: !cur.show,
                 coordination: { x: coord.left, y: coord.bottom },
-                //links will be an object with the key being the text of the current target and the value being the link
                 links: { ...links }
             }
         });
