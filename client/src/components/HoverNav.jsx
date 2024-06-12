5fd4a7792edfe50ab17bb340e30efd6979ce4f60
yokheved <yokheved.spr@gmail.com>
2024-06-12 22:05:22 +0300

    hover nav

--- client/src/components/HoverNav.jsx
+++ client/src/components/HoverNav.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function HoverNav({coordination, links}) {
+function HoverNav({coordination, links, onMouseLeave, onMouseEnter}) {
   return (
-    <nav className='hoverNav' style={{ top: `${coordination.y}px`, left: `${coordination.x}px` }}>
+    <nav className='hoverNav' style={{ top: `${coordination.y}px`, left: `${coordination.x}px` }}
+     onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <ul>
         {Object.entries(links).map(([key, value], i) => (
           <li key={i}>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,30 +6,36 @@ import HoverNav from './HoverNav';
 
 function NavBar({ userIn }) {
     const [hoverNav, setHoverNav] = useState({ show: false, coordination: { x: 0, y: 0 }, links: {} });
-    const handleHover = e => {
+    let hideTimeout;
+
+    const handleMouseEnter = (e) => {
+        clearTimeout(hideTimeout);
         const target = e.currentTarget;
-        e.preventDefault();
         const links = navlinks(userIn, target.innerText);
-        if (target.tagName != "A" || Object.keys(links).length === 0) return;
+        if (target.tagName !== "A" || Object.keys(links).length === 0) return;
         let coord = target.getBoundingClientRect();
-        setHoverNav(cur => {
-            return {
-                show: !cur.show,
-                coordination: { x: coord.left, y: coord.bottom },
-                links: { ...links }
-            }
+        setHoverNav({
+            show: true,
+            coordination: { x: coord.left, y: coord.bottom },
+            links: { ...links }
         });
-    }
+    };
+
+    const handleMouseLeave = (e) => {
+        hideTimeout = setTimeout(() => {
+            setHoverNav(cur => ({ ...cur, show: false }));
+        }, 200);
+    };
 
     return (
         <nav className='mainNav'>
-            <NavLink to={`/${userIn}`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>p.pic</NavLink>
-            <NavLink to={`/${userIn}/home`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>D-home</NavLink>
-            <NavLink to={`/${userIn}/about`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>About</NavLink>
-            <NavLink to={`/${userIn}/designs`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>Designs</NavLink>
-            <NavLink to={`/${userIn}/articles`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>Articles</NavLink>
-            <NavLink to={`/${userIn}/products`} onMouseOver={e => handleHover(e)} onMouseLeave={e => handleHover(e)}>Products</NavLink>
-            {hoverNav.show && <HoverNav coordination={hoverNav.coordination} links={hoverNav.links} />}
+            <NavLink to={`/${userIn}`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>p.pic</NavLink>
+            <NavLink to={`/${userIn}/home`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>D-home</NavLink>
+            <NavLink to={`/${userIn}/about`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>About</NavLink>
+            <NavLink to={`/${userIn}/designs`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>Designs</NavLink>
+            <NavLink to={`/${userIn}/articles`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>Articles</NavLink>
+            <NavLink to={`/${userIn}/products`} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}>Products</NavLink>
+            {hoverNav.show && <HoverNav coordination={hoverNav.coordination} links={hoverNav.links} onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)}/>}
         </nav>
     )
 }
