e5fdfdea3b159bd7c58a33de9e38b6eb9c35c4ed
Tiferet Maimon <tiferetmaimon@gmail.com>
2024-06-26 15:15:40 +0300

    גכעיחל

--- client/src/components/designs/newDesign/SideNav.jsx
+++ client/src/components/designs/newDesign/SideNav.jsx
@@ -7,8 +7,8 @@ function SideNav({height, width, depth, setHeight, setWidth, setDepth}) {
         <h1>Create a new design</h1>
         <h2>Change the size of the room</h2>
         <ChangeSize type="Height" size={height} setSize={setHeight} min={200} max={400}/>
-        <ChangeSize type="Width" size={width} setSize={setWidth} />
-        <ChangeSize type="Depth" size={depth} setSize={setDepth} />
+        <ChangeSize type="Width" size={width} setSize={setWidth} min={200} max={400}/>
+        <ChangeSize type="Depth" size={depth} setSize={setDepth} min={200} max={400}/>
       </nav>
   )
 }
