f42060615b1e00c8f3b1ff0ac558cc36c16d31f3
Tiferet Maimon <tiferetmaimon@gmail.com>
2024-06-30 13:15:59 +0300

    chace cleaning

--- server/api/middleware/authentication.js
+++ server/api/middleware/authentication.js
@@ -1,22 +1,27 @@
-import {decryptData, authenticateUser} from '../../services/authentication.js';
+import { decryptData, authenticateUser } from '../../services/authentication.js';
+
 const authenticate = async (req, res, next) => {
     // Get the token from the header
     const token = req.header('Authorization');
-    console.log("token at authenticate" + token);
+    console.log("Token at authenticate:", token);
+
     // Check if token exists
     if (!token) {
         return res.status(401).send({ error: 'No token provided' });
     }
+
     // Decrypt the token and authenticate the user
     try {
         const decoded = await decryptData(token);
-        console.log("decoded at authenticate",  decoded)
-        const {fullUser} = await authenticateUser(decoded);
+        console.log("Decoded token at authenticate:", decoded);
+
+        const { fullUser } = await authenticateUser(decoded);
         req.user = fullUser;
         next();
     } catch (error) {
-        res.status(401);
-        throw new Error("invalid at aytheticate");
+        console.error("Error during authentication:", error.message);
+        res.status(401).send({ error: 'Invalid token' });
     }
 };
+
 export default authenticate;
